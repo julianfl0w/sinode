7025a77d4a27f4839b0d6b387f018a77268dae42
Julian Loiacono <jcloiacon@gmail.com>
2023-01-25 17:44:19 -0500

    division and correction

---
+++ sinode/fractal_book.py
@@ -0,0 +1,416 @@
+import pickle
+import os
+from . import sinode
+here = os.path.dirname(os.path.abspath(__file__))
+
+def toPDF(intext, basedir = here, ext="html"):
+    preformat = intext
+    preformat = preformat.replace("/graphs", os.path.join(basedir, "graphs"))
+    preformat = preformat.replace("?raw=true", "")
+    # preformat = preformat.replace("<sup>", "")
+    # preformat = preformat.replace("</sup>", "")
+
+    formattedFilename = "README_formatted." + ext
+    with open(formattedFilename, "w+") as f:
+        f.write(preformat)
+
+    # os.system("mdpdf -o README.pdf README_formatted.md")
+    os.system("pandoc --pdf-engine=xelatex " + formattedFilename + " -s -o README.pdf")
+
+class FractalBook(sinode.Node):
+    def __init__(self, **kwargs):
+        # defaults
+        self.name = ""
+        self.priority = 1000
+        self.meta = sinode.Generic()
+        sinode.Node.__init__(self, **kwargs)
+        
+        if not os.path.exists("graphs"):
+            os.mkdir("graphs")
+        
+        # print("Ignore " + str(ignore))
+        if self.origin == "directory":
+            
+            self.name = self.source.split(os.sep)[-1]
+        
+            # read in ignore file
+            if os.path.exists(os.path.join(self.source, "ignore.py")):
+                with open(os.path.join(self.source, "ignore.py"), "r") as f:
+                    ignore = eval(f.read())
+            else:
+                ignore = []
+            
+            # iterate over files
+            for file in os.listdir(self.source):
+                resolved = os.path.join(self.source, file)
+                
+                # check for special files
+                if file in ignore or file == "ignore.py":
+                    continue
+                elif file == "meta.py":
+                    with open(resolved, 'r') as f:
+                        self.meta = eval(f.read())
+                        for k, v in self.meta.items():
+                            exec("self." + k + " = v")
+                    continue
+
+                # if its a dir, its a subcategory
+                if os.path.isdir(resolved):
+                    self.children += [FractalBook(parent = self, origin = "directory", source = resolved, depth=self.depth + 1)]
+
+                # otherwise, if it's a python file, execute it
+                # each python file is a chapter, containing a list of paragraphs
+                else:
+                    if file.endswith(".py"):
+                        print("processing file " + file)
+                        self.children += [FractalBook(parent = self, origin = "file", source = resolved, depth=self.depth + 1)]
+                        
+        elif self.origin == "file":
+            print("Adding chapter " + self.source)
+            self.name = self.source.split(os.sep)[-1].replace(".py", "")
+            with open(self.source, "r") as f:
+                print(self.source)
+                self.source = eval(f.read())
+                
+            self.processText()
+            
+        
+        elif self.origin == "text":
+            self.processText()
+            
+        # if it has no children, assume its a sentence
+        if not len(self.children):
+            self.origin = "sentence"
+        
+        self.computeHeight()
+        self.sortChildrenByPriority()
+            
+    def processText(self):
+        # if its a list, add all elements as children
+        if type(self.source) == list:
+            for element in self.source:
+                self.children += [FractalBook(parent = self, depth = self.depth+1, name = "", origin="text", source=element)]
+                if not hasattr(self.meta, "type"):
+                    self.meta.type == "list"
+        
+        # similar with dictionary, except the elements are named
+        if type(self.source) == dict:
+            for k, v in self.source.items():
+                # read meta, dont add as child
+                if k == "meta":
+                    self.meta = v
+                    for k, v in self.meta.items():
+                        exec("self." + k + " = v")
+                    continue
+                self.children += [FractalBook(parent = self, depth = self.depth+1, name = k, origin="text", source=v)]
+                    
+    def toMarkdown(self, **kwargs):
+        
+        for k, v in kwargs.items():
+            exec("self." + k + " = v")
+        
+        self.verseNo = 0
+        
+        markdownString = ""
+        htmlString = ""
+        
+        if self.ignore:
+            return {"html":htmlString, "markdown":markdownString}
+        
+        if self.depth == 0:
+            htmlString += "<html>\n"
+            htmlString += "<body style=\"color:white;align:justify\">\n"
+            
+        
+        if self.type == "lineage":
+            # put the graph title
+            markdownString += self.name + "\n"
+            htmlString += self.name + "\n"
+            # and include the graph!
+            self.toGraphViz(params=self.meta)
+            
+        elif self.type == "list":
+            # put the list title
+            markdownString += self.name + "\n"
+            htmlString += self.name + "\n"
+            htmlString += "<ul>"
+            
+            # start listing
+            for child in self.children:
+                markdownString += child.toList(depth = 0)["markdown"]
+                htmlString     += child.toList(depth = 0)["html"]
+                
+            htmlString += "</ul>"
+            # double newline after
+            markdownString += "\n\n"
+            htmlString     += "\n\n"
+
+        elif self.type == "eval":
+            #print(self.children[0].name)
+            htmlString += eval(self.children[0].children[0].name) + "\n"
+            
+        elif self.type == "default":
+            
+            
+            # if its height is 0, this is normal text
+            if self._height == 0:
+                
+                # print verse number and name
+                if hasattr(self.getApex(), "displayVerseNo"):
+                    markdownString += (
+                        "<sup>" + str(self.getApex().verseNo) + "</sup> "
+                    )
+                    htmlString += (
+                        "<sup>" + str(self.getApex().verseNo) + "</sup> "
+                    )    
+                    self.getApex().verseNo += 1
+                    
+                markdownString += self.name + ". "
+                htmlString += self.name + ". "
+                
+            # otherwise its a title / heading
+            else:
+                markdownString += "#" * (self.depth) + " " + self.name + "\n\n"
+                if self.depth == 0:
+                    htmlString += "<title>" + self.name + "</title>\n"
+                htmlString += ("<h" + str(self.depth+1) + ">" + 
+                                    "<a name=" + self.name.replace(" ","_") + ">" + 
+                                    self.name + 
+                                    "</a>"+
+                                    "</h" + str(self.depth+1) + ">\n")
+                self.getApex().verseNo = 0
+                #htmlString     += "\n<hr>"
+
+            # if its height is 1, add paragraph tag
+            if self._height == 1:
+                htmlString += (
+                    "<p align=\"justify\">"
+                )    
+                
+            # dont forget the children
+            for child in self.children:
+                markdownString += child.toMarkdown()["markdown"]
+                htmlString     += child.toMarkdown()["html"]
+
+            # add a new line between paragraphs
+            if self._height > 0:
+                # double newline after
+                markdownString += "\n\n"
+                htmlString     += "\n"
+                
+            # if its height is 1, add paragraph end
+            if self._height == 1:
+                htmlString += (
+                    "</p>"
+                )    
+        
+        else:
+            print(self.type)
+            die
+            
+        if self.depth == 0:
+            htmlString += "</body>\n"
+            htmlString += "</html>\n"
+            
+        return {"html":htmlString, "markdown":markdownString}
+
+
+    def asDotNotation(self, depth = 0, **kwargs):
+        dotNotationString = ""
+        
+        # create this key. its just a number. the label will be set later
+        self.boxNameWithQuotes = '"' + str(self.getApex().nodeNumber) + '"'
+        self.getApex().nodeNumber += 1
+        
+        kwargs["boxParams"]["label"] = '"' + self.name + '"'
+        #print(self.boxNameWithQuotes)
+        
+        if self.ignore:
+            return dotNotationString
+        
+        for key, value in kwargs.items():
+            exec(key + " = value")
+            
+
+        #print(kwargs)
+        # translate box parameters
+        dotNotationString += self.boxNameWithQuotes + " ["
+        for k, v in kwargs["boxParams"].items():
+            dotNotationString += k + "=" + str(v) + ", "
+        # remove final comma and space
+        dotNotationString = dotNotationString[:-2]
+        dotNotationString += "]\n"
+        
+        # relationships
+        if self.parent is not None and hasattr(self.parent, "boxNameWithQuotes"):
+            dotNotationString += self.parent.boxNameWithQuotes + " -> " + self.boxNameWithQuotes + " ["
+            for k, v in kwargs["arrowParams"].items():
+                dotNotationString += k + "=" + str(v) + ", "
+            # remove final comma and space
+            dotNotationString = dotNotationString[:-2]
+            dotNotationString += "]\n"
+                
+        # provided we havent reached max depth
+        if "maxDepth" in kwargs.keys() and depth >= kwargs["maxDepth"] :
+            return dotNotationString
+            
+        kwargs["depth"] = depth+1
+        # insert the child nodes
+        for c in self.children:
+            dotNotationString += c.asDotNotation(**kwargs)
+
+        return dotNotationString
+
+    def toGraphViz(self, **kwargs):
+        self.getApex().nodeNumber = 0
+        self.proc_kwargs(**kwargs)
+        print("graphing")
+        dotString = ""
+        dotString += "digraph D {\n"
+        
+        # apply default params if none exist
+        self.meta = {
+            "type": "lineage",
+            "graphParams": {
+                "rankdir": "LR",
+                "style": "filled",
+                "color": "lightgrey",
+                "bgcolor": "\"#262626\"",
+                "fillcolor": "\"darkgray:gold\"",
+                "gradientangle": 0,
+                "dpi": 300,
+            },
+            "boxParams": {
+                "shape": "box",
+                #"color": "black",
+                "color": "\"#262626\"",
+                "fontcolor": "white",
+                #"fillcolor": "\"darkorchid4:grey10\"",
+                "fillcolor": "\"#6C2944:#29001C\"",
+                "style": "filled",
+                "gradientangle": 270.05,
+            },
+            "arrowParams":{
+                "color": "white",
+                "penwidth": 1,
+            }
+        }
+        
+        # translate graph parameters
+        for k, v in self.meta["graphParams"].items():
+            dotString += k + " = " + str(v) + "\n"
+        # insert our own node
+        #kwargs["boxParams"] = self.meta["boxParams"]
+        dotString += self.asDotNotation(arrowParams = self.meta["arrowParams"], 
+                                        boxParams = self.meta["boxParams"],**kwargs)
+        dotString += "}"
+
+        imagename = os.path.join("graphs", self.name.replace(" ", "_") + ".png")
+        
+        # dont regenerate graphs if indicated
+        if hasattr(self.getApex(), "skipGraphs") and self.getApex().skipGraphs:
+            pass
+        else:
+            # write out the dot notation file
+            filename = os.path.join("graphs", self.name.replace(" ", "_") + ".dot")
+            with open(filename, "w+") as f:
+                f.write(dotString)
+
+            # convert to image
+            runstring = "dot -Tpng '" + filename + "' -o " + "'" + imagename + "'"
+            #print(runstring)
+            os.system(runstring)
+
+        # reference it in the markdown
+        markdownString = (
+            "\n![" + self.name + "](/" + imagename + '?raw=true""' + self.name + '")\n\n'
+        )
+
+        # and HTML
+        htmlString = "<img src=\"" + imagename + "\" width=\"100%\">\n"
+
+        return {"html":htmlString, "markdown":markdownString}
+
+    def toList(self, depth=0):
+        markdownString = ""
+        markdownString += "  " * depth
+        markdownString += "- "
+        markdownString += self.name + "\n"
+        
+        # do children markdown
+        for c in self.children:
+            markdownString +=  c.toList(depth=depth + 1)["markdown"]
+
+        # make self as item
+        htmlString = ""
+        htmlString += "<li>\n"
+        htmlString += self.name
+        
+        # create a list of children, if applicable
+        if len(self.children):
+            htmlString += "<ul>\n"
+            for c in self.children:
+                htmlString += c.toList(depth=depth + 1)["html"]
+            htmlString += "</ul>\n"
+
+        # end self as item
+        htmlString += "</li>"
+        
+        return {"html":htmlString, "markdown":markdownString}
+
+    def toTableOfContents(self, depth=0, **kwargs):
+        for key, value in kwargs.items():
+            exec(key + " = value")
+        kwargs["depth"] = depth + 1
+            
+        markdownString = ""
+        htmlString = ""
+        
+        if depth > kwargs["maxDepth"]:
+            return {"html":htmlString, "markdown":markdownString}
+        
+        markdownString += "  " * depth
+        markdownString += "- "
+        markdownString += self.name + "\n"
+        
+        # do children markdown
+        for c in self.children:
+            markdownString +=  c.toTableOfContents(**kwargs)["markdown"]
+
+        # make self as item
+        htmlString += "<li>\n"
+        if hasattr(self.getApex(), "link"):
+            htmlString += "<a href=#" + self.name.replace(" ", "_") + ">" + self.name + "</a>"
+        else:
+            htmlString += self.name
+        
+        # create a list of children, if applicable
+        if len(self.children):
+            htmlString += "<ul>\n"
+            for c in self.children:
+                htmlString += c.toTableOfContents(**kwargs)["html"]
+            htmlString += "</ul>\n"
+
+        # end self as item
+        htmlString += "</li>"
+        
+        return {"html":htmlString, "markdown":markdownString}
+
+
+if __name__ == "__main__":
+    m = FractalBook(os.path.join(here, "Book Of Julian"), depth=1)
+
+    preformat = m.toMarkdown()
+    with open("README.md", "w+") as f:
+        f.write(preformat)
+    die
+    preformat = preformat.replace("/graphs", os.path.join(here, "graphs"))
+    preformat = preformat.replace("?raw=true", "")
+    # preformat = preformat.replace("<sup>", "")
+    # preformat = preformat.replace("</sup>", "")
+
+    with open("README_formatted.md", "w+") as f:
+        f.write(preformat)
+
+    # os.system("mdpdf -o README.pdf README_formatted.md")
+    os.system("pandoc --pdf-engine=xelatex README_formatted.md -s -o README.pdf")
--- sinode/sinode.py
+++ sinode/sinode.py
@@ -1,12 +1,14 @@
-import dill as pickle
+import pickle
 import os
+here = os.path.dirname(os.path.abspath(__file__))
+
 
 class Generic(object):
     def __init__(self, **kwargs):
         self.children = []
         self.priority = 999
         self.ignore = False
-        self.type = "normal"
+        self.type = "default"
         self.proc_kwargs(**kwargs)
     def proc_kwargs(self, **kwargs):
         for key, value in kwargs.items():
@@ -104,131 +106,6 @@ class Node(Generic):
             # dump information to that file
             pickle.dump(self, f)
 
-    def asDotNotation(self, depth = 0, **kwargs):
-        self.dotNotationString = ""
-        
-        # create this key
-        self.boxNameWithQuotes = '"' + self.name + '"'
-        print(self.boxNameWithQuotes)
-        
-        if self.ignore:
-            return self.dotNotationString
-        
-        for key, value in kwargs.items():
-            exec(key + " = value")
-            
-
-        print(kwargs)
-        # translate box parameters
-        self.dotNotationString += self.boxNameWithQuotes + " ["
-        for k, v in kwargs["boxParams"].items():
-            self.dotNotationString += k + "=" + str(v) + ", "
-        # remove final comma and space
-        self.dotNotationString = self.dotNotationString[:-2]
-        self.dotNotationString += "]\n"
-        
-        # relationships
-        if self.parent is not None and hasattr(self.parent, "boxNameWithQuotes"):
-            self.dotNotationString += self.parent.boxNameWithQuotes + " -> " + self.boxNameWithQuotes + " ["
-            for k, v in kwargs["arrowParams"].items():
-                self.dotNotationString += k + "=" + str(v) + ", "
-            # remove final comma and space
-            self.dotNotationString = self.dotNotationString[:-2]
-            self.dotNotationString += "]\n"
-                
-        # provided we havent reached max depth
-        if "maxDepth" in kwargs.keys() and depth >= kwargs["maxDepth"] :
-            return self.dotNotationString
-            
-        kwargs["depth"] = depth+1
-        # insert the child nodes
-        for c in self.children:
-            self.dotNotationString += c.asDotNotation(**kwargs)
-
-        return self.dotNotationString
-
-    def asList(self, depth=0):
-        string = ""
-        string += "  " * depth
-        string += "- "
-        string += self.name + "\n"
-
-        for i in self.children:
-            string += i.asList(depth=depth + 1)
-
-        return string
-
-    def toMarkdown(self):
-        self.markdownString = ""
-        
-        # print verse number and name
-        self.markdownString += (
-            "<sup>" + str(self.getApex().verseNo) + "</sup> " + self.name + ". "
-        )
-        self.getApex().verseNo += 1
-
-        return self.markdownString
-
-    def toGraphViz(self, **kwargs):
-        self.proc_kwargs(**kwargs)
-        print("graphing")
-        dotString = ""
-        dotString += "digraph D {\n"
-        
-        # apply default params if none exist
-        self.meta = {
-            "type": "lineage",
-            "graphParams": {
-                "rankdir": "LR",
-                "style": "filled",
-                "color": "lightgrey",
-                "bgcolor": "\"#262626\"",
-                "fillcolor": "\"darkgray:gold\"",
-                "gradientangle": 0,
-            },
-            "boxParams": {
-                "shape": "box",
-                #"color": "black",
-                "color": "\"#262626\"",
-                "fontcolor": "white",
-                #"fillcolor": "\"darkorchid4:grey10\"",
-                "fillcolor": "\"#6C2944:#29001C\"",
-                "style": "filled",
-                "gradientangle": 270.05,
-            },
-            "arrowParams":{
-                "color": "white",
-                "penwidth": 1,
-            }
-        }
-        
-        # translate graph parameters
-        for k, v in self.meta["graphParams"].items():
-            dotString += k + " = " + str(v) + "\n"
-        # insert our own node
-        #kwargs["boxParams"] = self.meta["boxParams"]
-        dotString += self.asDotNotation(arrowParams = self.meta["arrowParams"], 
-                                        boxParams = self.meta["boxParams"],**kwargs)
-        dotString += "}"
-
-        # write out the dot notation file
-        filename = os.path.join("graphs", self.name.replace(" ", "_") + ".dot")
-        with open(filename, "w+") as f:
-            f.write(dotString)
-
-        # convert to image
-        imagename = os.path.join("graphs", self.name.replace(" ", "_") + ".png")
-        runstring = "dot -Tpng '" + filename + "' -o " + "'" + imagename + "'"
-        print(runstring)
-        os.system(runstring)
-
-        # reference it in the markdown
-        self.markdownString = (
-            "\n![" + self.name + "](/" + imagename + '?raw=true "' + self.name + '")\n\n'
-        )
-
-        return self.markdownString
-
     def getDecendentGenerationCount(self):
         if self.children == []:
             return 0
@@ -261,19 +138,6 @@ class Node(Generic):
             return self
         return self.parent.getApex()
     
-    def toPDF(self):
-        preformat = self.toMarkdown()
-        preformat = preformat.replace("/graphs", os.path.join(here, "graphs"))
-        preformat = preformat.replace("?raw=true", "")
-        # preformat = preformat.replace("<sup>", "")
-        # preformat = preformat.replace("</sup>", "")
-
-        with open("README_formatted.md", "w+") as f:
-            f.write(preformat)
-
-        # os.system("mdpdf -o README.pdf README_formatted.md")
-        os.system("pandoc --pdf-engine=xelatex README_formatted.md -s -o README.pdf")
-
     def computeHeight(self):
         if not len(self.children):
             self._height = 0
@@ -281,6 +145,14 @@ class Node(Generic):
             self._height = 1 + max([c.computeHeight() for c in self.children])
             
         return self._height
+    
+    def fromAbove(self, key):
+        if hasattr(self, key):
+            return self.key
+        elif self.parent is not None:
+            return self.parent.fromAbove(key)
+        else:
+            raise Exception("Key not found above")
         
 def NodeFromFile(filename):
     # open a file, where you ant to store the data
@@ -319,140 +191,3 @@ class Minode(Node):
             return self
         return self.parent.getAncestor(ancestorType)
 
-
-class FractalBook(Node):
-    def __init__(self, **kwargs):
-        # defaults
-        self.name = ""
-        self.priority = 1000
-        Node.__init__(self, **kwargs)
-        
-        if not os.path.exists("graphs"):
-            os.mkdir("graphs")
-        
-        # print("Ignore " + str(ignore))
-        if self.origin == "directory":
-            
-            self.name = self.source.split(os.sep)[-1]
-        
-            # read in ignore file
-            if os.path.exists(os.path.join(self.source, "ignore.py")):
-                with open(os.path.join(self.source, "ignore.py"), "r") as f:
-                    ignore = eval(f.read())
-            else:
-                ignore = []
-
-            # list files by priority
-            
-                
-            # iterate over files
-            for file in os.listdir(self.source):
-                resolved = os.path.join(self.source, file)
-                
-                # check for special files
-                if file in ignore or file == "ignore.py":
-                    continue
-                elif file == "meta.py":
-                    with open(resolved, 'r') as f:
-                        self.meta = eval(f.read())
-                        for k, v in self.meta.items():
-                            exec("self." + k + " = v")
-                    continue
-
-                # if its a dir, its a subcategory
-                if os.path.isdir(resolved):
-                    self.children += [FractalBook(parent = self, origin = "directory", source = resolved, depth=self.depth + 1)]
-
-                # otherwise, if it's a python file, execute it
-                # each python file is a chapter, containing a list of paragraphs
-                else:
-                    if file.endswith(".py"):
-                        print("processing file " + file)
-                        self.children += [FractalBook(parent = self, origin = "file", source = resolved, depth=self.depth + 1)]
-                        
-        elif self.origin == "file":
-            print("Adding chapter " + self.source)
-            self.name = self.source.split(os.sep)[-1].replace(".py", "")
-            with open(self.source, "r") as f:
-                print(self.source)
-                self.source = eval(f.read())
-                
-            self.processText()
-            
-        
-        elif self.origin == "text":
-            self.processText()
-            
-        # if it has no children, assume its a sentence
-        if not len(self.children):
-            self.origin = "sentence"
-        
-        self.computeHeight()
-        self.sortChildrenByPriority()
-            
-    def processText(self):
-        if type(self.source) == list:
-            raise Exception("Lists not allowed!")
-            #for i, paragraph in enumerate(chapter):
-            #    self.children += [FractalBook(parent = self, origin="text", source=paragraph)]
-        if type(self.source) == dict:
-            for k, v in self.source.items():
-                if k == "meta":
-                    self.meta = v
-                    for k, v in self.meta.items():
-                        exec("self." + k + " = v")
-                    continue
-                self.children += [FractalBook(parent = self, depth = self.depth+1, name = k, origin="text", source=v)]
-                    
-    def toMarkdown(self):
-        self.verseNo = 0
-        self.markdownString = ""
-        
-        if self.ignore:
-            return self.markdownString
-
-        self.markdownString += self.name + "\n"
-        if self.type == "lineage":
-            self.markdownString += self.toGraphViz(params=self.meta)
-        elif self.type == "list":
-            for child in self.children:
-                self.markdownString += child.asList(depth = 0   )
-            self.markdownString += "\n\n"
-
-
-        else:
-            # add its title
-            if self._height > 0:
-                self.markdownString += "#" * (self.depth) + " " + self.name + "\n"
-                self.getApex().verseNo = 0
-
-            else:
-                self.markdownString += self.name + ". "
-
-            for child in self.children:
-                self.markdownString += child.toMarkdown()
-
-            if self._height >= 1:
-                # add a new line between paragraphs
-                self.markdownString += "\n\n"
-            
-        return self.markdownString
-
-
-if __name__ == "__main__":
-    m = FractalBook(os.path.join(here, "Book Of Julian"), depth=1)
-
-    preformat = m.toMarkdown()
-    with open("README.md", "w+") as f:
-        f.write(preformat)
-    die
-    preformat = preformat.replace("/graphs", os.path.join(here, "graphs"))
-    preformat = preformat.replace("?raw=true", "")
-    # preformat = preformat.replace("<sup>", "")
-    # preformat = preformat.replace("</sup>", "")
-
-    with open("README_formatted.md", "w+") as f:
-        f.write(preformat)
-
-    # os.system("mdpdf -o README.pdf README_formatted.md")
-    os.system("pandoc --pdf-engine=xelatex README_formatted.md -s -o README.pdf")
